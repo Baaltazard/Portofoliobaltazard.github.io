0651dc6eaeedb0870f6c4ef394969454eab9c1ef
Angga <153361050+Baaltazard@users.noreply.github.com>
2024-07-31 08:32:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,31 @@ menuicon.onclick = () => {
   menuicon.classList.toggle('bx-x');
   navbar.classList.toggle('active');
 };
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw5QTQc1fwFIcpWnpmlPqM682452uu4JbF8_iQTgjmmTvnV541L17NM7aJ1rZR8fH5N9w/exec';
+const form = document.forms['myform'];
+const btnkirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
+const myAlert = document.querySelector('.my-alert');
+const ContactForm = document.querySelector('.myform');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  btnLoading.classList.toggle('hidden');
+  btnkirim.classList.toggle('hidden');
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then((response) => {
+      btnLoading.classList.toggle('hidden');
+      btnkirim.classList.toggle('hidden');
+      myAlert.classList.toggle('hidden');
+      
+      form.reset();
+      console.log('Success!', response);
+    })
+    .catch((error) => console.error('Error!', error.message));
+});
+
+closeIcon = document.querySelector('.close');
+closeIcon.addEventListener('click', () => {
+  myAlert.classList.toggle('hidden');
+});
